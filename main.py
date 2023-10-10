97974abb64ac693d5b61e0b5084496fdb61316d7
OmPython <147528570+OmPython@users.noreply.github.com>
2023-10-10 22:37:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-p = int(input('Enter the principal amount'))
-r = int(input('Enter the rate of interest per annum'))
-n = int(input('Enter the number of years'))
+p = int(input('Enter the principal amount : '))
+r = int(input('Enter the rate of interest per annum : '))
+n = int(input('Enter the number of years : '))
 
 si = (p*n*r)/100
+print('Simple Interest = ')
 print(si)
